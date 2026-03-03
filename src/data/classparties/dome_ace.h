3a7452cd0dd5c261e5ba7d820b5c92d1ce58c8b2
Zachary Wilmer <43282058+theartiztzachary@users.noreply.github.com>
2026-03-02 19:06:08 -0500

    made all the files

---
+++ src/data/classparties/dome_ace.h
@@ -0,0 +1,10 @@
+//probably won't be used?
+
+{
+    .species = SPECIES_GEODUDE,
+    .gender = TRAINER_MON_RANDOM_GENDER,
+    .iv = TRAINER_PARTY_IVS(0, 0, 0, 0, 0, 0),
+    .lvl = 21,
+    .nature = NATURE_HARDY,
+    .dynamaxLevel = MAX_DYNAMAX_LEVEL,
+},
